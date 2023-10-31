18ec1dfb7624225cb1c993470a912dd2c3d8ba7f
Artem <zezebolz@gmail.com>
2023-10-31 20:44:28 +0300

    auth setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div>
-      <p className="text-sky-500 text-3xl">Hello messenger!</p>
+    <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-7 bg-gray-100">
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <Image
+          src="/images/logo.png"
+          height={48}
+          width={48}
+          alt="Logo"
+          className="mx-auto w-auto"
+        />
+
+        <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+          Sign in to your account
+        </h2>
+      </div>
+      {/* AuthFrom */}
     </div>
   );
 }
